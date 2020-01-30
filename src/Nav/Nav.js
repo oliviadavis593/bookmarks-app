55006267981bb1165c55498851ab7b7b0ab1ce1e
Olivia Davis <olivia.davis593@gmail.com>
2020-01-30 14:48:49 -0600

    React router implementation

--- src/Nav/Nav.js
+++ src/Nav/Nav.js
@@ -1,15 +1,15 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 
 export default function Nav(props) {
   return (
     <nav className='Nav'>
-      <button onClick={() => props.clickPage('list')}>
-        Bookmark List
-      </button>
-      {' '}
-      <button onClick={() => props.clickPage('add')}>
+      <Link to={'/'}>
+        BookMark List
+      </Link>
+      <Link to={'/add-bookmark'}>
         Add Bookmark
-      </button>
+      </Link>
     </nav>
   );
 }
