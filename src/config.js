03970ed7a8a3c4e8b63632d77b88d1d967f878f0
Olivia Davis <olivia.davis593@gmail.com>
2020-02-29 11:14:43 -0600

    implement security for API_KEY

--- src/config.js
+++ src/config.js
@@ -1,4 +1,5 @@
 export default {
   API_ENDPOINT: `https://tf-ed-bookmarks-api.herokuapp.com/v3/bookmarks`,
-  API_KEY: '$2a$10$wg8dJlA1LQbsAfhyu/IcIeAA1I3odvCU/3F3LYKo23ZNgVdIN.k8C',
+  //API_KEY: '$2a$10$wg8dJlA1LQbsAfhyu/IcIeAA1I3odvCU/3F3LYKo23ZNgVdIN.k8C',
+  API_KEY: process.env.REACT_APP_API_KEY
 }
